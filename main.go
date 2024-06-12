e356a8dea7a7614bc0a5689be6473cde23520720
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-12 07:47:12 +0200

    update user struct and handlers to include IsChirpyRed field

--- main.go
+++ main.go
@@ -54,6 +54,8 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
 
+	mux.HandleFunc("POST /api/polka/webhooks", apiCfg.handlerWebhook)
+
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
 	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
 	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
